cd87c24554ab9786cfb64e311889e054016d4d6c
jinhao <105051513@qq.com>
2023-09-27 14:44:43 +0800

    修改db文件访问方式

--- js/index.js
+++ js/index.js
@@ -38,7 +38,8 @@ $(function () {
 
   // ============================数据请求业务==============================
   $.getJSON(
-    'https://raw.githubusercontent.com/Hyukoh7/Hyukoh7.github.io/main/data/db.json',
+    // 'https://raw.githubusercontent.com/Hyukoh7/Hyukoh7.github.io/main/data/db.json',
+      '../data/db.json',
     res => {
       // 页面渲染
       var categoryHTML = template('CategoryId', {
